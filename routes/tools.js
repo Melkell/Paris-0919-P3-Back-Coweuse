d7dfbacff183f737fd7f2e38c085965f2eedfd46
Melkell <mel.kelly.maisonnave@gmail.com>
2019-12-16 16:40:22 +0100

    route tools

--- routes/tools.js
+++ routes/tools.js
@@ -7,13 +7,52 @@ const connect = require('../sql')
 
 // Get all tools
 router.get('/', (req, res) => {
-    res.status(200).send('Tools list')
+    connect.query('SELECT equipement.id, equipement.name, exploitation.name as exploitation_name, sous_type.name as sous_type_name FROM equipement INNER JOIN exploitation ON exploitation.id=exploitation_id INNER JOIN sous_type ON sous_type.id=sous_type_id;', (err, results) => {
+        if (err) {
+            res.sendStatus(500)
+        }
+        else {
+            res.json(results)
+        }
+    })
 })
 
 // Get a tool by ID
 router.get('/:id', (req, res) => {
-    const id = req.params.id
-    res.status(200).send(`Tool n°${id}`)
+    connect.query('SELECT equipement.id, equipement.name, exploitation.name as exploitation_name, sous_type.name as sous_type_name FROM equipement INNER JOIN exploitation ON exploitation.id=exploitation_id INNER JOIN sous_type ON sous_type.id=sous_type_id WHERE equipement.id=?', id, (err, results) => {
+        if (err) {
+            res.sendStatus(500)
+        }
+        else {
+            res.json(results)
+        }
+    })
 })
+
+router.post('/add', (req, res) => {
+    const userData = req.body
+    connect.query('INSERT INTO equipement SET ?', userData, (err) => {
+        if (err) {
+            res.status(500).send('Sorry, a problem has occurred while adding this user.')
+        }
+        else {
+            res.status(200).send('Tool successfully added.')
+        }
+    })
+})
+
+router.delete('/:id', (req, res) => {
+    const id = req.params.id
+    connect.query('DELETE FROM equipement WHERE equipement.id=?', id, (err) => {
+        if (err) {
+            res.status(500).send('Sorry, a problem has occurred while removing this user.')
+        }
+        else {
+            res.status(200).send('Tool successfully removed.')
+        }
+    })
+})
+
+
 
 module.exports = router
