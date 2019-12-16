d6934bb4b4b32b316db60f590901028816e93721
FlorentCmb <florent.cmb77@gmail.com>
2019-12-16 16:23:25 +0100

    Fixing bugs and add delete and post queries

--- routes/user.js
+++ routes/user.js
@@ -7,7 +7,7 @@ const connect = require('../sql')
 
 // GET all users
 router.get('/', (req, res) => {
-    connect.query('SELECT *, role.name as role_name, exploitation.name as exploitation_name FROM utilisateur INNER JOIN role ON role_id=role.id INNER JOIN exploitation ON id_exploitation=exploitation.id;', (err, results) => {
+    connect.query('SELECT utilisateur.id, utilisateur.first_name, utilisateur.last_name, utilisateur.email, utilisateur.active, role.name as role_name, exploitation.name as exploitation_name FROM utilisateur INNER JOIN role ON role_id=role.id INNER JOIN exploitation ON id_exploitation=exploitation.id;', (err, results) => {
         if (err) {
             res.sendStatus(500)
         }
@@ -20,8 +20,7 @@ router.get('/', (req, res) => {
 // GET an user by ID
 router.get('/:id', (req, res) => {
     const id = req.params.id
-    console.log(id)
-    connect.query('SELECT *, role.name as role_name, exploitation.name as exploitation_name FROM utilisateur INNER JOIN role ON role_id=role.id INNER JOIN exploitation ON id_exploitation=exploitation.id WHERE utilisateur.id=?;', id, (err, results) => {
+    connect.query('SELECT utilisateur.id, utilisateur.first_name, utilisateur.last_name, utilisateur.email, utilisateur.active, role.name as role_name, exploitation.name as exploitation_name FROM utilisateur INNER JOIN role ON role_id=role.id INNER JOIN exploitation ON id_exploitation=exploitation.id WHERE utilisateur.id=?', id, (err, results) => {
         if (err) {
             res.sendStatus(500)
         }
@@ -36,11 +35,23 @@ router.post('/add', (req, res) => {
     const userData = req.body
     connect.query('INSERT INTO utilisateur SET ?', userData, (err) => {
         if (err) {
-            console.log(err)
             res.status(500).send('Sorry, a problem has occurred while adding this user.')
         }
         else {
             res.status(200).send('User successfully added.')
+        }
+    })
+})
+
+// DELETE an user
+router.delete('/:id', (req, res) => {
+    const id = req.params.id
+    connect.query('DELETE FROM utilisateur WHERE id=?', id, (err) => {
+        if (err) {
+            res.status(500).send('Sorry, a problem has occurred while removing this user.')
+        }
+        else {
+            res.status(200).send('User successfully removed.')
         }
     })
 })
