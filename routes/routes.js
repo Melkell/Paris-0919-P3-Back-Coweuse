dac62d4e15cdc457ec41a0f34640cfdb07fb0b97
Mickaelchoipro <mickaelchoipro@gmail.com>
2020-01-09 17:54:25 +0100

    post-update-delete for user

--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,22 @@
 const router = require('express').Router();
 const user = require('./user');
 const exploitation = require('./exploitation');
-const parcelle = require('./parcelle');
-const production = require('./production')
 
-router.get('/user', user.getUsers);
+// const parcelle = require('./parcelle');
+// const production = require('./production')
+
+// Dashboard admin
 router.get('/exploitation/:id', exploitation.getExploitationById)
 router.get('/exploitation/:id/parcelles', exploitation.getNumberParcellesByExploitationId)
-router.get('/exploitation/:id/productions', exploitation.getNumberProductionsByEploitationId)
+router.get('/exploitation/:id/productions', exploitation.getNumberProductionsByExploitationId)
+router.get('/exploitation/:id/users', exploitation.getNumberUsersByExploitationId)
+
+// Ressources admin
+router.get('/user/create', user.createUser);
+router.get('/user/update', user.updateUser);
+router.get('/user/delete', user.deleteUser);
+
+
 // router.put('/user', user.updateUserById);
 // router.put('/production', production.updateProductionById)
 // router.use('/production', production)
--- routes/user.js
+++ routes/user.js
@@ -1,86 +1,47 @@
 const connection = require('../conf');
 
-// GET all users
-const getUsers = (req, res) => {
-  connection.query('SELECT * FROM user', (err, results) => {
+// POST a new user
+const createUser = (req, res) => {
+  const userData = req.body
+
+  connection.query('INSERT INTO user SET ?', userData, (err, results) => {
     if (err) {
-      res.status(500).send('Erreur lors de la récupération des users')
+      res.status(500).send("Erreur lors de l'ajout du user")
     } else {
       res.json(results)
     }
   });
 }
 
-// // GET a user by id
-// router.get('/:id', (req, res) => {
-//   const id = req.params.id;
+// MODIFY a user
+const updateUser = (req, res) => {
+  const userId = req.params.id;
+  const userData = req.body;
 
-//   connection.query('SELECT * FROM user WHERE id = ?', id, (err, results) => {
-//     if (err) {
-//       res.status(500).send('Erreur lors de la récupération du user')
-//     } else {
-//       res.json(results)
-//     }
-//   });
-// });
+  connection.query('UPDATE user SET ? WHERE id = ?', [userData, userId], (err, results) => {
+    if (err) {
+      res.status(500).send("Erreur lors de la modification du user");
+    } else {
+      res.status(200).send('User modifié');
+    }
+  });
+}
 
-// // GET a user's mission
-// router.get('/:user_id/mission', (req, res) => {
-//   const userId = req.params.user_id;
+// DELETE a user
+const deleteUser = (req, res) => {
+  const userId = req.params.id;
 
-//   connection.query('SELECT * FROM mission_user WHERE user_id = ? ', userId, (err, results) => {
-//     if (err) {
-//       console.log(err)
-//       res.status(500).send('Erreur lors de la récupération de la mission du user')
-//     } else {
-//       res.json(results)
-//     }
-//   });
-// });
-
-// // POST to add a new user
-// router.post('/add', (req, res) => {
-//   const userData = req.body;
-  
-//   connection.query('INSERT INTO user SET ?', userData, (err, results) => {
-//     if (err) {
-//       res.status(500).send("Erreur lors de l'ajout du user")
-//     } else {
-//       res.status(201).send(`User ${userData.first_name} ${userData.last_name} added`)
-//     }
-//   });
-// });
-
-// // MODIFY a user
-// router.put('/:id/modify', (req, res) => {
-//   const userId = req.params.id;
-//   const userData = req.body;
-
-//   connection.query('UPDATE user SET ? WHERE id = ?', [userData, userId], (err, results) => {
-//     if (err) {
-//       res.status(500).send("Erreur lors de la modification du user");
-//     } else {
-//       res.status(200).send('User modifié');
-//     }
-//   });
-// });
-
-
-// // DELETE a user
-// router.delete('/delete/:id', (req, res) => {
-//   const deleteId = req.params.id;
-
-//   connection.query('DELETE FROM user WHERE id = ?', deleteId, (err, results) => {
-//     if (err) {
-//       console.log(err)
-//       res.status(500).send('Erreur lors de la suppression du user')
-//     } else {
-//       res.status(200).send(`Le user a bien été supprimé`)
-//     }
-//   });
-// });
-
+  connection.query('DELETE FROM user WHERE id = ?', userId, (err, results) => {
+    if (err) {
+      res.status(500).send('Erreur lors de la suppression du user')
+    } else {
+      res.status(200).send(`Le user a bien été supprimé`)
+    }
+  });
+}
 
 module.exports = {
-  getUsers
+  createUser,
+  updateUser,
+  deleteUser
 }
